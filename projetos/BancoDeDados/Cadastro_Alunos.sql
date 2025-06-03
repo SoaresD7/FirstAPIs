8ed1b9eee63cbbea5a92b2afe6fedd4c49f50acb
Davi Soares <Davi.soares2008@icloud.com>
2025-06-03 16:47:23 -0300

    API dia 03/06
    Falta integrar a API com o HTML

--- projetos/BancoDeDados/Cadastro_Alunos.sql
+++ projetos/BancoDeDados/Cadastro_Alunos.sql
@@ -1,2 +1,9 @@
-create database Cadastro_Alunos;
+create database cadastro_Alunos;
 use Cadastro_Alunos;
+
+-- Criação da tabela aluno
+CREATE TABLE alunos (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
